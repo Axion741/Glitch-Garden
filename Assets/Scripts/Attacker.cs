034a7e8d4c573b06353bfb9d37d5cec053da6395
Unknown <k.ballagher@gmail.com>
2017-06-20 12:50:53 +0100

    Added Triggers + Rigidbodies, Started Animation Events

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class Attacker : MonoBehaviour {
 
     [Range (-1f, 1.5f)]
-    public float walkSpeed;
+    public float currentSpeed;
 
 	// Use this for initialization
 	void Start () {
-		
+        Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
+        myRigidbody.isKinematic = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+        transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(name + " trigger enter");
+    }
+
+    public void SetSpeed (float speed)
+    {
+        currentSpeed = speed;
+    }
+
+    public void StrikeCurrentTarget (float damage)
+    {
+        Debug.Log(name + " is attacking for " + damage + " damage");
+    }
 }
